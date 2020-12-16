1f0220868c5c2bf20d17b9b28a55d64ceacfe6a8
sueokatakaaki <smilepom@yahoo.co.jp>
2020-12-16 22:33:42 +0900

    abe

--- app/src/main/java/com/example/ourapplication/MainActivity.java
+++ app/src/main/java/com/example/ourapplication/MainActivity.java
@@ -36,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present...
-        // abc
+        // abcde
+        // abe
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
